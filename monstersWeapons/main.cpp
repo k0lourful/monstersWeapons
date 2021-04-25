a2afe903e3d9999e310790cc2eef86f51d229925
Daniel White <xt1zer@outlook.com>
2021-04-25 03:53:13 +0300

    Fix game return code check

--- monstersWeapons/main.cpp
+++ monstersWeapons/main.cpp
@@ -3,7 +3,7 @@
 int main() {
     Game game;
     int retCode = game.Run();
-    if (!retCode)
+    if (retCode)
         std::cout << "Game shut down incorrectly. Error code: " << retCode << "\n\n";
 
     return 0;
