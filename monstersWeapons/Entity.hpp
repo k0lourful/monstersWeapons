9ba11665a5f8dcfda4dff9ad7a20422da2b48e7c
Daniel White <xt1zer@outlook.com>
2021-04-25 03:50:16 +0300

    Add entity type; add get type, health and check if alive methods

--- monstersWeapons/Entity.hpp
+++ monstersWeapons/Entity.hpp
@@ -2,15 +2,22 @@
 class Weapon;
 
 class Entity {
+public:
+    enum TYPE { PLAYER, WEAKMONSTER, NORMALMONSTER, STRONGMONSTER };
+
 protected:
     int health;
     int damage;
     Weapon* weapon;
+    TYPE type;
 
 public:
-    Entity(const int& h, const int& d, Weapon* w) : health(h), damage(d), weapon(w) {}
+    Entity(const int& h, const int& d, Weapon* w, const TYPE& t) : health(h), damage(d), weapon(w), type(t) {}
     ~Entity() { if (weapon) delete weapon; }
 
+    TYPE getType() const { return type; }
+    int getHealth() const { return health; }
+    bool isAlive() const { return health > 0; }
     void reduceHealth(const int& dmg) { health -= dmg; }
     virtual int attack(Entity*) = 0;
 };
